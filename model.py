60d0f4ab1743fde94a526afd46e5e825c29afecf
TimLlanto <45229539+pyTimK@users.noreply.github.com>
2022-05-22 00:18:27 +0800

    Fixed import error

--- model.py
+++ model.py
@@ -1,28 +1,29 @@
 import tensorflow as tf
-from tensorflow.keras import Sequential, layers
+from tensorflow import keras
 from constants import img_height, img_width
 
 num_classes = 2
 
 data_augmentation = keras.Sequential([
-    layers.RandomFlip("horizontal", input_shape=(img_height, img_width, 3)),
-    layers.RandomRotation(0.1),
-    layers.RandomZoom(0.1),
+    keras.layers.RandomFlip("horizontal",
+                            input_shape=(img_height, img_width, 3)),
+    keras.layers.RandomRotation(0.1),
+    keras.layers.RandomZoom(0.1),
 ])
 
-model = Sequential([
+model = keras.Sequential([
     data_augmentation,
-    layers.Rescaling(1. / 255),  # Normalization layer
-    layers.Conv2D(16, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(32, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.2),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(num_classes)
+    keras.layers.Rescaling(1. / 255),  # Normalization layer
+    keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+    keras.layers.MaxPooling2D(),
+    keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+    keras.layers.MaxPooling2D(),
+    keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+    keras.layers.MaxPooling2D(),
+    keras.layers.Dropout(0.2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(num_classes)
 ])
 
 model.compile(optimizer='adam',
